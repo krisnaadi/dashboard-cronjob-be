d4fcb62fcbe872761e972859d305e0c7ff15b29b
krisnaadi <man.krisna@gmail.com>
2025-01-27 10:29:57 +0800

    feat(log): add log repository

---
+++ internal/repository/postgre/log/log.go
@@ -0,0 +1,62 @@
+package log
+
+import (
+	"context"
+
+	"github.com/krisnaadi/dashboard-cronjob-be/internal/entity"
+	"github.com/krisnaadi/dashboard-cronjob-be/pkg/logger"
+)
+
+func (repository *Repository) GetLogByID(ctx context.Context, ID int64) (entity.Log, error) {
+	var log entity.Log
+
+	err := repository.db.Where("id = ?", ID).Find(&log).Error
+	if err != nil {
+		logger.Trace(ctx, struct{ ID int64 }{ID}, err, "repository.db.Where().Find() error - GetLogByID")
+		return entity.Log{}, err
+	}
+
+	return log, nil
+}
+
+func (repository *Repository) GetLogs(ctx context.Context) ([]entity.Log, error) {
+	var categories []entity.Log
+	err := repository.db.Order("id asc").Find(&categories).Error
+
+	if err != nil {
+		logger.Trace(ctx, nil, err, "repository.db.Order().Find() error - GetLogs")
+		return nil, err
+	}
+
+	return categories, nil
+}
+
+func (repository *Repository) InsertLog(ctx context.Context, log entity.Log) (entity.Log, error) {
+	err := repository.db.Create(&log).Error
+	if err != nil {
+		logger.Trace(ctx, log, err, "repository.db.Create() error - InsertLog")
+		return entity.Log{}, err
+	}
+
+	return log, nil
+}
+
+func (repository *Repository) UpdateLog(ctx context.Context, log entity.Log) (entity.Log, error) {
+	err := repository.db.Save(&log).Error
+	if err != nil {
+		logger.Trace(ctx, log, err, "repository.db.Save() error - UpdateLog")
+		return entity.Log{}, err
+	}
+
+	return log, nil
+}
+
+func (repository *Repository) DeleteLog(ctx context.Context, ID int64) error {
+	err := repository.db.Delete(&entity.Log{}, ID).Error
+	if err != nil {
+		logger.Trace(ctx, struct{ ID int64 }{ID}, err, "repository.db.Delete() error - DeleteLog")
+		return err
+	}
+
+	return nil
+}
---
+++ internal/repository/postgre/log/repository.go
@@ -0,0 +1,27 @@
+package log
+
+import (
+	"context"
+
+	"github.com/krisnaadi/dashboard-cronjob-be/internal/entity"
+
+	"gorm.io/gorm"
+)
+
+type RepositoryProvider interface {
+	GetLogs(ctx context.Context) ([]entity.Log, error)
+	GetLogByID(ctx context.Context, ID int64) (entity.Log, error)
+	InsertLog(ctx context.Context, log entity.Log) (entity.Log, error)
+	UpdateLog(ctx context.Context, log entity.Log) (entity.Log, error)
+	DeleteLog(ctx context.Context, ID int64) error
+}
+
+type Repository struct {
+	db *gorm.DB
+}
+
+func New(db *gorm.DB) RepositoryProvider {
+	return &Repository{
+		db: db,
+	}
+}
